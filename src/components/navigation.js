a1ca6747d96b82d4765fabe18d5dfc1044d45703
niin4 <riekkin@gmail.com>
2022-03-29 22:03:43 +0300

    Starter stuff

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,12 +7,12 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Kahden kerroksen kohinaa</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
-          Home
+          Etusivu
         </Link>
       </li>
       <li className={styles.navigationItem}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ class RootIndex extends React.Component {
       <Layout location={this.props.location}>
         <Hero
           image={author.heroImage.gatsbyImageData}
-          title={author.name}
-          content={author.shortBio.shortBio}
+          title={'Kahden kerroksen kohinaa'}
+          content={'Maalia ja listoja maaseudulla'}
         />
         <ArticlePreview posts={posts} />
       </Layout>
